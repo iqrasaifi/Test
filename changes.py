5da1d5ed32c704160c2f6d80fb91c9b35fcff355
Iqra Saifi <iqrasaifi20900@gmail.com>
2020-06-05 22:11:31 +0530

    changes.py

--- changes.py
+++ changes.py
@@ -4,9 +4,12 @@ print('It is a 1st change')
 
 #change 2 online(pull)
 print('It is a 2nd change')
+<<<<<<< HEAD
 
 print('Hello World')
 =======
 #change 1
 print('It is a 1st change')
 >>>>>>> parent of 59c096c... Update changes.py
+=======
+>>>>>>> parent of e461d8e... changes.py
