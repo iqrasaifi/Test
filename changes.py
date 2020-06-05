bdafb64e4064eefcf887cc87bd74edb78a6eeb5e
Iqra Saifi <iqrasaifi20900@gmail.com>
2020-06-05 22:10:54 +0530

    Update changes.py

--- changes.py
+++ changes.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #change 1 local(push)
 print('It is a 1st change')
 
@@ -5,3 +6,7 @@ print('It is a 1st change')
 print('It is a 2nd change')
 
 print('Hello World')
+=======
+#change 1
+print('It is a 1st change')
+>>>>>>> parent of 59c096c... Update changes.py
